95d1a115086919a15297008f4d09618f0be4ad72
btpeach <btpeach@umich.edu>
2019-04-08 15:59:50 -0400

    Add file for main loop and util dir for all other functions

---
+++ src/dso_main.m
@@ -0,0 +1,10 @@
+% build vector of frame structs containing point cloud param and others
+
+% loop through each frame
+
+%   front-end
+%   back-end
+%   save pose
+% end loop
+
+% compare poses to GT and plot results
