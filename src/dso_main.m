618dc20cc693037fd3bcf94d23d4ea5f0805b548
Jeffrey Dominic <jeffrey@voxel51.com>
2019-04-10 13:24:54 -0400

    updated dso_main with simplified structure

--- src/dso_main.m
+++ src/dso_main.m
@@ -12,67 +12,55 @@
 % ============================================
 
 global window
-max_num_keyframes = 7;
+max_num_keyframes = 3;
 
 % Holds a cell array of frame structs
 window.keyframes = cell(1, max_num_keyframes);
-% Specify the maximum index of frames
-window.maxFrameIdx = 1;
-% Dictionary of active points and their locations in each frame
-%   KeyType - index of active point
-%   ValueType - [(x_1, y_1), (x_2, y_2), ...] (location of active point
-%       in each frame, where (x_n, y_n) is the location of the active point
-%       in frame n. If (x, y) = (-1, 1), then active point not
-%       observable in the frame.
-window.activePoints = containers.Map('KeyType', 'int32', 'ValueType', 'Any');
-% Array of transformation matrices'
-window.transform = cell(max_num_keyframes, max_num_keyframes);
+window.maxFrameIdx = 0;
+% Array of transformation matrices
+%   - Index 1 -> matrix between 1 and 2
+%   - Index 2 -> matrix between 2 and 3
+%   - Index 3 -> matrix between 1 and 3
+window.transform = cell(1, max_num_key_frames);
 
 % ============================================
 % FRAME STRUCT (an element of window)
-% ============================================
-
-% Create elements for every frame
-num_candidate_points = 2000;
-for i=1:max_num_keyframes
-    % Storing candidate points in a cell array. The cell array must be
-    % ordered by minimum distance to active points (as mentioned in
-    % the Google Doc). Each element of this array is a 1 x 2 double
-    % array [x_1, y_1], where (x_1, y_1) is the x, y position of the
-    % candidate point in the frame.
-    window.keyframes{i}.candidatePoints = cell(1, num_candidate_points);
-    % distanceToPrevious will hold the frame's distance to
-    % the previous frame
-    window.keyframes{i}.distanceToPrevious = 0;
-end
-
+% Properties:
+%   - candidatePoints (array of point locations)
+%   - frameIdx (index of frame for accessing point cloud)
 % ============================================
 
 % feeding frame by frame
 for i=1:num_frames
     %decide if a frame could be a keyframe  
-    if i == 1
-        window.keyframes{window.maxFrameIdx}.candidatePoints = ...
-            findCandidatePoints(uw_livingRoomData{i}.Location);
-        window.maxFrameIdx = window.maxFrameIdx +1;
-    elseif i == 2
-        window.keyframes{window.maxFrameIdx}.candidatePoints = ...
-            findCandidatePoints(uw_livingRoomData{i}.Location);
-        window.keyframes{window.maxFrameIdx}.distanceToPrevious = ...
-            distanceBetweenFrames(window.maxFrameIdx + 1,...
-                                  window.maxFrameIdx); % will be implemented
+    if i == 1 
+        % findCandidatePoints -> SIFT
         window.maxFrameIdx = window.maxFrameIdx + 1;
+        window.keyframes{window.maxFrameIdx}.candidatePoints = ...
+            findCandidatePoints(freiburg{i}.Color);
+        % Store the frame index (to access corresponding point cloud)
+        window.keyframes{window.maxFrameIdx}.frameIdx = i;
     else
-        if isKeyFrame(frames{i}) %need to be implemented
-            window.keyframes{window.maxFrameIdx}.candidatePoints = ...
-                findCandidatePoints(uw_livingRoomData{i}.Location);
-            window.keyframes{window.maxFrameIdx}.distanceToPrevious = ...
-                distanceBetweenFrames(window.maxFrameIdx + 1,...
-                                      window.maxFrameIdx);
-            window.maxFrameIdx = window.maxFrameIdx +1;
+        if isKeyFrame() %need to be implemented
+            % -- These 2 lines should be in isKeyFrame()
+            %window.maxFrameIdx = window.maxFrameIdx + 1;
+            %window.keyframes{window.maxFrameIdx}.candidatePoints = ...
+            %    findCandidatePoints(freiburg{i}.Color);
+            window.keyframes{window.maxFrameIdx}.frameIdx = i;
+        else
+            continue
         end
     end
-  
+    
+    % If three frames are in window, marginalize the oldest one
+    if window.maxFrameIdx == max_num_keyframes
+        window.transform{3} = window.transform{1} * window.transform{2};
+        back_end();
+        window.keyframes(1:2) = window.keyframes(2:3);
+        window.maxFrameIdx = window.maxFrameIdx - 1;
+    end
+        
+
     % calculate transformation from each keyframe to all other keyframes
     for j = 1: window.maxFrameIdx
         % make rkhs registration object
