23df457b7e671be9552f6e0f64db043044bee6c4
Huiwen Xu <huiwenx@umich.edu>
2019-04-16 19:28:39 -0400

    dynamic dvo

--- src/dvo/main_func/joint_dso_main.m
+++ src/dvo/main_func/joint_dso_main.m
@@ -1,223 +1,203 @@
-clc; clear; close all
+clear; close all
 
 
 
 %% load point cloud
 freiburg2 = load('freiburg2.mat');
 freiburg2 = freiburg2.freiburg2;
+% load key information
+%%%%% to do: KEYS
+KEYS = cell(2,1);
+KEYS{1} = cell(6,1);
+KEYS{1}{1}(1) = 1;
+KEYS{1}{1}(2) = 2;
+KEYS{1}{2}(1) = 2;
+KEYS{1}{2}(2) = 3;
+KEYS{1}{3}(1) = 3;
+KEYS{1}{3}(2) = 4;
+KEYS{1}{4}(1) = 1;
+KEYS{1}{4}(2) = 3;
+KEYS{1}{5}(1) = 2;
+KEYS{1}{5}(2) = 4;
+KEYS{1}{6}(1) = 1;
+KEYS{1}{6}(2) = 4;
 
+
+KEYS{2} = cell(4,1);
+KEYS{2}{1}(1) = 2;
+KEYS{2}{1}(2) = 3;
+KEYS{2}{2}(1) = 3;
+KEYS{2}{2}(2) = 4;
+KEYS{2}{3}(1) = 4;
+KEYS{2}{3}(2) = 5;
+KEYS{2}{4}(1) = 2;
+KEYS{2}{4}(2) = 5;
+
+
+%%%%% to do: N = window size
+N = 4;
+
+% init 
 POSE = cell(length(freiburg2),1);
 POSE{1} = eye(4);
 COORD = cell(length(freiburg2),1);
 COORD{1} = pose_to_coord(POSE{1});
 
-for i = 1:length(freiburg2)-2
+for i = 1:length(KEYS)
     
     tic
     
-    % 1-2, 2-3, 1-3
-    ptCloud1 = freiburg2{i};
-    ptCloudMove = freiburg2{i+1};
-    ptCloud3 = freiburg2{i+2};
-    dvo = rgbd_dvo();
-    R_init = eye(3);
-    T_init = zeros(3,1);
-    tform12 = affine3d([R_init, T_init; 0, 0, 0, 1]');
-    tform23 = affine3d([R_init, T_init; 0, 0, 0, 1]');
-    tform13 = affine3d([R_init, T_init; 0, 0, 0, 1]');
-    %% downsample
-    % downsample point clouds using grids
+    Z = cell(length(KEYS{i}),1);
     
-    percen_range = 0.04:0.02:0.08;
-    if ~isempty(POSE{i+1})
-        disp('already calculated')
-        tform12.T = (POSE{i}\POSE{i+1})';
-        fprintf('%d-%d Transformation Estimate:\n',i,i+1)
-        disp(tform12.T')
-    else
-        for percen = percen_range
-            fprintf('%d-%d\n',i,i+1)
-            fprintf('%f resolution\n',percen)
-            % moving - fixed : 1-2
-            fixed1 = [];
-            fixed1.ptcloud = removeInvalidPoints(pcdownsample(ptCloudMove, 'random', percen));
-            fixed1.image = rgb2gray(ptCloudMove.Color);
-            moving1 = removeInvalidPoints(pcdownsample(ptCloud1, 'random', percen));
-            % make rkhs registration object
-            dvo = rgbd_dvo();
-            dvo.set_ptclouds(fixed1, moving1);
-            dvo.tform = tform12;
-            dvo.align();
-            tform12 = dvo.tform;
-            fprintf('%d-%d Transformation Estimate:\n',i,i+1)
+    beg_idx = KEYS{i}{1}(1);
+    end_idx = beg_idx + N - 1;
+    
+    ptCloudbeg = freiburg2{beg_idx};
+    ptCloudend = freiburg2{end_idx};
+    
+    for j = 1:length(KEYS{i})
+        R_init = eye(3);
+        T_init = zeros(3,1);
+        tform12 = affine3d([R_init, T_init; 0, 0, 0, 1]');
+        
+        fidx1 = KEYS{i}{j}(1);
+        fidx2 = KEYS{i}{j}(2);
+
+        ptCloud1 = freiburg2{fidx1};
+        ptCloud2 = freiburg2{fidx2};
+        
+        % downsample point clouds
+        percen_range = 0.04:0.02:0.08;
+        if ~isempty(POSE{fidx2})
+            disp('Already Calculated')
+            tform12.T = (POSE{fidx1}\POSE{fidx2})';
+            Z{j} = tform12.T';
+            fprintf('%d-%d Transformation Estimate:\n',fidx1,fidx2)
             disp(tform12.T')
+        else
+            for percen = percen_range
+                fprintf('%d-%d\n',fidx1,fidx2)
+                fprintf('%f resolution\n',percen)
+                % moving - fixed : 1-2
+                fixed1 = [];
+                fixed1.ptcloud = removeInvalidPoints(pcdownsample(ptCloud2, 'random', percen));
+                fixed1.image = rgb2gray(ptCloud2.Color);
+                moving1 = removeInvalidPoints(pcdownsample(ptCloud1, 'random', percen));
+                % make rkhs registration object
+                dvo = rgbd_dvo();
+                dvo.set_ptclouds(fixed1, moving1);
+                dvo.tform = tform12;
+                dvo.align();
+                tform12 = dvo.tform;
+                fprintf('%d-%d Transformation Estimate:\n',fidx1,fidx2)
+                disp(tform12.T')
+            end
+            Z{j} = tform12.T';
         end
     end
-
-    for percen = percen_range
-        fprintf('%d-%d\n',i+1,i+2)
-        fprintf('%f resolution\n',percen)
-        % moving - fixed : 2-3
-        fixed2 = [];
-        fixed2.ptcloud = removeInvalidPoints(pcdownsample(ptCloud3, 'random', percen));
-        fixed2.image = rgb2gray(ptCloud3.Color);
-        moving2 = removeInvalidPoints(pcdownsample(ptCloudMove, 'random', percen));
-        % make rkhs registration object
-        dvo = rgbd_dvo();
-        dvo.set_ptclouds(fixed2, moving2);
-        dvo.tform = tform23;
-        dvo.align();
-        tform23 = dvo.tform;
-        fprintf('%d-%d Transformation Estimate:\n',i+1,i+2)
-        disp(tform23.T')
-    end
-
-    for percen = percen_range
-        fprintf('%d-%d\n',i,i+2)
-        fprintf('%f resolution\n',percen)
-        % moving - fixed : 1-3
-        fixed3 = [];
-        fixed3.ptcloud = removeInvalidPoints(pcdownsample(ptCloud3, 'random', percen));
-        fixed3.image = rgb2gray(ptCloud3.Color);
-        moving3 = removeInvalidPoints(pcdownsample(ptCloud1, 'random', percen));
-        % make rkhs registration object
-        dvo = rgbd_dvo();
-        dvo.set_ptclouds(fixed3, moving3);
-        dvo.tform.T = (tform12.T' * tform23.T')';
-        dvo.align();
-        tform13 = dvo.tform;
-        fprintf('%d-%d Transformation Estimate:\n',i,i+2)
-        disp(tform13.T')
-    end
-
-
+    
     % joint optimization 
-    Z_pre = cell(1,3);
-    Z_pre{1} = tform12.T';
-    Z_pre{2} = tform23.T';
-    Z_pre{3} = tform13.T';
-    Keys = cell(1,3);
-    Keys{1} = [1,2];
-    Keys{2} = [2,3];
-    Keys{3} = [1,3];
-    % pose is not the same as twist!
-    % pose{1} = eye, pose{2} = tf(1->2), pos{3} = tf(1->3)
-    jointPose = joint_optimization(Z_pre, Keys);
-    pose2 = jointPose{2};
-    pose3 = jointPose{3};
-
-    % update transformation
-    tform12.T = pose2';
-    tform23.T = (pose2\pose3)';
-    tform13.T = pose3';
-    fprintf('%d-%d Joint Optimization Transformation Estimate:\n',i,i+2)
-%     disp(tform12.T')
-%     disp(tform23.T')
-%     disp(tform13.T')
-
-    POSE{i} = POSE{i};
-    POSE{i+1} = POSE{i}*pose2;
-    POSE{i+2} = POSE{i}*pose3;
-    
-    % pose to coord
-    COORD{i} = pose_to_coord(POSE{i});
-    COORD{i+1} = pose_to_coord(POSE{i+1});
-    COORD{i+2} = pose_to_coord(POSE{i+2});
-    
-
-    % transform 1 to 3
-    ptCloudtransformed1 = pctransform(ptCloud1,tform13);
-
-    mergeSize = 0.015;
-    ptCloudScene = pcmerge(ptCloud3, ptCloudtransformed1, mergeSize);
+    fprintf('%d-%d Joint Optimization Transformation\n',beg_idx,end_idx)
+    jointPose = joint_optimization(Z, KEYS{i}, N);
+    for k = beg_idx:end_idx
+        POSE{k} = POSE{beg_idx} * jointPose{k-(beg_idx-1)};
+        COORD{k} = pose_to_coord(POSE{k});
+    end
     
     toc
     
-%     % Visualize the input images.
-%     figure(1)
-%     subplot(2,2,1)
-%     imshow(ptCloud3.Color)
-%     title('First input image')
-%     drawnow
-% 
-%     subplot(2,2,3)
-%     imshow(ptCloud1.Color)
-%     title('Second input image')
-%     drawnow
-% 
-%     % Visualize the world scene.
-%     subplot(2,2,[2,4])
-%     pcshow(ptCloudScene, 'VerticalAxis','Y', 'VerticalAxisDir', 'Down')
-%     title('Initial world scene')
-%     xlabel('X (m)')
-%     ylabel('Y (m)')
-%     zlabel('Z (m)')
-%     drawnow
+    % transform beg to end
+    tformbe = affine3d((POSE{beg_idx}\POSE{end_idx})');
+    ptCloudtransformedbeg = pctransform(ptCloudbeg,tformbe);
+
+    mergeSize = 0.015;
+    ptCloudScene = pcmerge(ptCloudend, ptCloudtransformedbeg, mergeSize);
     
     
+    
+    % Visualize the input images.
+    figure(1)
+    subplot(2,2,1)
+    imshow(ptCloudend.Color)
+    title('First input image')
+    drawnow
+
+    subplot(2,2,3)
+    imshow(ptCloudbeg.Color)
+    title('Second input image')
+    drawnow
+
+    % Visualize the world scene.
+    subplot(2,2,[2,4])
+    pcshow(ptCloudScene, 'VerticalAxis','Y', 'VerticalAxisDir', 'Down')
+    title('Initial world scene')
+    xlabel('X (m)')
+    ylabel('Y (m)')
+    zlabel('Z (m)')
+    drawnow
 end
 
-%% trajectory
-temp = 357;
-X = zeros(temp,1);
-Y = zeros(temp,1);
-Z = zeros(temp,1);
-
-
-for i = 1:temp
-%     coor = pose_to_coord(POSEL{i})
-%     X(i) = coor(1);
-%     Y(i) = coor(2);
-%     Z(i) = coor(3);
-    X(i) = COORD{i}(1);
-    Y(i) = COORD{i}(2);
-    Z(i) = COORD{i}(3);
-end
-
-
+% %% trajectory
+% temp = 357;
+% X = zeros(temp,1);
+% Y = zeros(temp,1);
+% Z = zeros(temp,1);
+% 
+% 
+% for i = 1:temp
+% %     coor = pose_to_coord(POSEL{i})
+% %     X(i) = coor(1);
+% %     Y(i) = coor(2);
+% %     Z(i) = coor(3);
+%     X(i) = COORD{i}(1);
+%     Y(i) = COORD{i}(2);
+%     Z(i) = COORD{i}(3);
+% end
+% 
+% 
 % 
 % A = [X, Y, Z];
 % GT = load('GT.mat');
 % xyz = GT.xyz;
 % xyz = xyz*10;
 % 
-% xyz(:,1) = xyz(:,1) - xyz(1,1);
-% xyz(:,2) = xyz(:,2) - xyz(1,2);
-% xyz(:,3) = xyz(:,3) - xyz(1,3);
-
-
-
-figure(4)
-plot3(X,Y,Z)
-hold on
-plot3(COORD_2(:,1),COORD_2(:,2),COORD_2(:,3))
-hold off
-legend('direct','indirect')
-
-
-
-%% Stitch a Sequence of Point Clouds
-
-PtCloud1 = freiburg2{1};
-for i = 2:357
-%     ptCloudCurrent = ptcloud_edge_filter(freiburg2{i});
-    ptCloud2 = freiburg2{i};
-    
-    tform21 = affine3d(POSES_2{i}^(-1)'); % direct
-%     tform21 = affine3d(poses{i}^(-1)'); % indirect
-    
-    ptCloudtransformed2 = pctransform(ptCloud2, tform21);
-
-    mergeSize = 0.015;
-    PtCloud1 = pcmerge(PtCloud1, ptCloudtransformed2, mergeSize);
-
-end
-
-% Visualize the world scene.
-figure(5)
-pcshow(PtCloud1, 'VerticalAxis','Y', 'VerticalAxisDir', 'Down')
-title('world scene')
-xlabel('X (m)')
-ylabel('Y (m)')
-zlabel('Z (m)')
-drawnow
+% % xyz(:,1) = xyz(:,1) - xyz(1,1);
+% % xyz(:,2) = xyz(:,2) - xyz(1,2);
+% % xyz(:,3) = xyz(:,3) - xyz(1,3);
+% 
+% 
+% 
+% figure(4)
+% % plot3(X,Y,Z)
+% % hold on
+% plot3(xyz(:,1),xyz(:,2),xyz(:,3))
+% % hold off
+% legend('direct','gt')
+% 
+% 
+% 
+% %% Stitch a Sequence of Point Clouds
+% 
+% PtCloud1 = freiburg2{1};
+% for i = 2:100
+% %     ptCloudCurrent = ptcloud_edge_filter(freiburg2{i});
+%     ptCloud2 = freiburg2{i};
+%     
+%     tform21 = affine3d(POSE{i}^(-1)'); % direct
+% %     tform21 = affine3d(poses{i}^(-1)'); % indirect
+%     
+%     ptCloudtransformed2 = pctransform(ptCloud2, tform21);
+% 
+%     mergeSize = 0.015;
+%     PtCloud1 = pcmerge(PtCloud1, ptCloudtransformed2, mergeSize);
+% 
+% end
+% 
+% % Visualize the world scene.
+% figure(5)
+% pcshow(PtCloud1, 'VerticalAxis','Y', 'VerticalAxisDir', 'Down')
+% title('world scene')
+% xlabel('X (m)')
+% ylabel('Y (m)')
+% zlabel('Z (m)')
+% drawnow
--- src/dvo/util/joint_optimization.m
+++ src/dvo/util/joint_optimization.m
@@ -4,9 +4,11 @@ function jointPose = joint_optimization(Z, Keys, N)
 %        N is window size
 % output: twist between connected nodes in a window
 
+idx_off = Keys{1}(1)-1;
+
 % measurement noise covariance
-% Sigma = diag([0.05^2, 0.03^2, 0.03^2, 0.05^2, 0.05^2, 0.05^2]);
-Sigma = eye(6);
+Sigma = diag([0.05^2, 0.03^2, 0.03^2, 0.05^2, 0.05^2, 0.05^2]);
+% Sigma = eye(6);
 % Cholesky factor of covariance for sampling
 Lz = chol(Sigma, 'lower');
 
@@ -19,19 +21,19 @@ end
 % Jacobian matrix
 A = zeros(6 + 6 * length(Z), 6 * length(T_init));
 % right hand side (residuals)
-b = zeros(6 + 6 * length(Z),1);
+dx = zeros(6 + 6 * length(Z),1);
 % anchor node covariance; we want to fix the node so the covariance should
 % be small. This will result in large weights in the optimization process.
-% Sigma_init = eye(6) * 0.1^2;
-% A(1:6,1:6) = chol(Sigma_init, 'lower') \ eye(6);
-A(1:6,1:6) = eye(6);
+Sigma_init = eye(6) * 0.1^2;
+A(1:6,1:6) = chol(Sigma_init, 'lower') \ eye(6);
+% A(1:6,1:6) = eye(6);
 
 % Gauss-Newton solver over SE(3)
 T_est = T_init;
 max_iter = 100;
 iter = 0;
 eps_Jr = 1e-9;
-dx = zeros(6 + 6 * length(Z),1);
+
 % SE(3) right Jacobian inverse and adjoint
 invJr = @RightJacobianInverse_SE3;
 Ad = @Adjoint_SE3;
@@ -44,11 +46,11 @@ while iter < max_iter
     for i = 1:length(Keys)
         key = Keys{i};
         res_idx = 6*i+1:6*i+6;
-        r(res_idx, 1) =  wedge(logm(Z{i} \ (T_est{key(1)} \  T_est{key(2)})));
+        r(res_idx, 1) =  wedge(logm(Z{i} \ (T_est{key(1)-idx_off} \  T_est{key(2)-idx_off})));
         % fill in Jacobian of the corresponding target position
-        idx = 6 * key(1) - 5;
-        A(res_idx, idx:idx+5) = Lz \ -invJr(r(res_idx, 1)) * Ad(T_est{key(2)} \  T_est{key(1)});
-        idx = 6 * key(2) - 5;
+        idx = 6 * (key(1)-idx_off) - 5;
+        A(res_idx, idx:idx+5) = Lz \ -invJr(r(res_idx, 1)) * Ad(T_est{key(2)-idx_off} \  T_est{key(1)-idx_off});
+        idx = 6 * (key(2)-idx_off) - 5;
         A(res_idx, idx:idx+5) = Lz \ invJr(r(res_idx, 1));
         r(res_idx, 1) =  Lz \ r(res_idx, 1);
     end
@@ -56,13 +58,12 @@ while iter < max_iter
     % solve normal equations
     Jr = -A' * r;
     dx = (A' * A) \ Jr;
-    
     % retract and update the estimate
     for i = 1:length(T_est)
         xi = dx(6*i-5:6*i);
         T_est{i} = T_est{i} * expm(hat(xi));
+        
     end
-
     
     % check if converged
     if norm(Jr) < eps_Jr
