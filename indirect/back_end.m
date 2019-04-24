71efa8c37ec4d3198f7b7363a3a14e67472f39dd
btpeach <btpeach@umich.edu>
2019-04-23 22:45:03 -0400

    Clean up comments

--- indirect/back_end.m
+++
@@ -1,1 +0,0 @@
-% call external functions in ./util/
--- test/indirect_main.m
+++ test/indirect_main.m
@@ -1,17 +1,5 @@
 clc, clear, close all;
-% build vector of frame structs containing point cloud param and others
-
-% loop through each frame
-
-%   front-end
-%   back-end
-%   save pose
-% end loop
 global poses;
-% ============================================
-% WINDOW STRUCT
-% ============================================
-
 global window
 
 % This will now be a dynamic variable.
@@ -23,33 +11,17 @@ window.keyframes = cell(1, max_num_keyframes);
 window.kframe_indices = [];
 
 window.maxFrameIdx = 0;
-% Array of transformation matrices
-%   - Index 1 -> matrix between 1 and 2
-%   - Index 2 -> matrix between 2 and 3
-%   - Index 3 -> matrix between 1 and 3
 window.transform = cell(max_num_keyframes, max_num_keyframes);
 % Connections will be a binary adjacency matrix that will be converted
 % into a 'Keys' variable to pass to joint_optimization
 window.connections = zeros(max_num_keyframes, max_num_keyframes);
-% Threshold must be < 0.5647 (0.3 works well)
 window.flowThresh = 0.2;
-% There must be at least 20 non-outlier matches
-% between keyframes
-
-%window.minNumMatches = 120;
 window.minNumMatches = 120;
 window.connectionThresh = 80;
 
 window.minimum = 400;
 
-% ============================================
-% FRAME STRUCT (an element of window)
-% Properties:
-%   - candidatePoints (array of point locations)
-%   - frameIdx (index of frame for accessing point cloud)
-% ============================================
-
-% feeding frame by frame
+% load dataset
 global freiburg2;
 freiburg2 = load('freiburg2.mat');
 freiburg2 = freiburg2.freiburg;
@@ -98,52 +70,6 @@ for i=1:num_frames
     
     % If three frames are in window, marginalize the oldest one
     if window.maxFrameIdx == max_num_keyframes
-%         window.num_keyframes = window.num_keyframes + 1;
-%         features = window.keyframes{window.maxFrameIdx}.candidatePoints.features;
-%         points = window.keyframes{window.maxFrameIdx}.candidatePoints.points;
-%         keyframe_features = window.keyframes{window.maxFrameIdx - 2}.candidatePoints.features;
-%         keyframe_points = window.keyframes{window.maxFrameIdx - 2}.candidatePoints.points;
-%         index_pair = matchFeatures(features, keyframe_features);
-%         matchedPoints_frame = points(index_pair(:, 1));
-%         matchedPoints_keyframe = keyframe_points(index_pair(:, 2));
-%         frame_idx = window.keyframes{window.maxFrameIdx}.frameIdx;
-%         key_idx = window.keyframes{window.maxFrameIdx - 2}.frameIdx;
-%       
-%         [~, in_dist, in_orig] = estimateGeometricTransform(...
-%             matchedPoints_frame, matchedPoints_keyframe, 'projective');
-%         
-%         loc_frame = round(in_dist.Location);
-%         loc_keyframe = round(in_orig.Location);
-%         % Construct point cloud using location of matched_points
-%         ptcloud_frame = zeros(size(loc_frame, 1), 3);
-%         ptcloud_keyframe = zeros(size(loc_keyframe, 1), 3);
-%    
-%         for j=1:size(loc_frame, 1)
-%             ptcloud_frame(j, :) = freiburg2{frame_idx}.Location(...
-%                 loc_frame(j, 2), loc_frame(j, 1), :);
-%             ptcloud_keyframe(j, :) = freiburg2{key_idx}.Location(...
-%                 loc_keyframe(j, 2), loc_keyframe(j, 1), :);
-%         end
-        %{
-        rows_nan_frame = any(isnan(ptcloud_frame'));
-        rows_nan_keyframe = any(isnan(ptcloud_keyframe'));
-        rows_remove = rows_nan_frame | rows_nan_keyframe;
-        ptframe = ptcloud_frame(~rows_remove, :);
-        ptkey = ptcloud_keyframe(~rows_remove, :);
-        %}
-        % Compute initial transformation matrix between point clouds
-        % from keyframe to frame
-%         tform = findInitialTform(ptcloud_frame, ptcloud_keyframe);
-%         window.transform{3} = tform;
-%         
-%         det(window.transform{1})
-%         det(window.transform{2})
-%         det(window.transform{3})
-%         
-%         if det(window.transform{3}) < 0.99
-%             display("here")
-%         end
-        
         % Find non-zero elements in adjacency matrix
         G = graph(window.connections, 'upper');
         connected_nodes = G.Edges.EndNodes;
